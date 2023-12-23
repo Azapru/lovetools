db95e151ef0f88eec693504eca903015b37650a4
Azapru <62551802+Azapru@users.noreply.github.com>
2023-12-23 21:33:46 +0100

    update install script bcuz windows is dumb

--- INSTALL.bat
+++ INSTALL.bat
@@ -7,7 +7,7 @@ echo Installing lovetools...
 
 mkdir %APPDATA%\lovetools
 copy dist\lovetools.exe %APPDATA%\lovetools\lovetools.exe
-setx PATH "%PATH%;%APPDATA%\lovetools"
+start explorer %APPDATA%\lovetools
 
 echo Finished!
 pause
