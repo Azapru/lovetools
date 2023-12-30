5a3154e4323d6c6982fdbde7b19b0a5e236fe07a
Azapru <62551802+Azapru@users.noreply.github.com>
2023-12-30 19:11:01 +0100

    Release

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def help():
     print("project run\t\t\tRuns this LÖVE project, works only on projects setup by lovetools.")
 
 def version():
-    print("lovetools v0.1.2git | Python "+str(sys.version))
+    print("lovetools v0.2.0stable | Python "+str(sys.version))
 
 def project_setup(loveVer):
     print("lovetools - Setting up LÖVE project in the current directory.\n")
